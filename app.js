07d111ce08d2625318b4ab0b4adfdff100a0b9b9
DhanushSaiCoder <dhanushsai1467@gmail.com>
2024-12-04 10:37:07 +0530

    +header +headerNavBtns

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const mongoose = require('mongoose')
 const app = express()
 require('dotenv').config();
 const path = require('path')
+
+
 mongoose.connect(`${process.env.MONGO_URI}`)
     .then(console.log('connected to db'))
     .catch((err) => {
         console.error('error connecting db', err)
     })
 
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.redirect('/home')
 })
-app.get('/home',(req,res) => {
+app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, './public/pages/index.html'));
 })
 app.listen(process.env.PORT, () => {
